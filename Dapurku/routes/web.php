2e1d94ab6a1e76f846811478aeaf217a8bcfcc26
GalaxyDust02 <121703948+GalaxyDust02@users.noreply.github.com>
2024-05-06 21:10:15 +0700

    c

--- Dapurku/routes/web.php
+++ Dapurku/routes/web.php
@@ -1,25 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/laravel', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('landing');
-});
+//Landing page
+Route::get('/',[HomeController::class,'Home']);
 
-#auth
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
-
-#konten
-Route::get('dashboard', [UserController::class, 'dashboard'])->name('dashboard');
-
-
-
+//Authintikasi
+Route::get('/login',[SessionController::class,'masuk']);
